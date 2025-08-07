9c390d07a43edf5cc19ec060f63fc8aa4ebf6c55
Edvalz <eduardo_valllejo@outlook.es>
2025-08-07 11:40:06 -0600

    feat: se actualizan las dependencias en los package

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,31 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
-import Box from '@mui/material/Box';
-
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+import Box from "@mui/material/Box";
 
 export default function Home() {
   return (
-    <Box component="section" sx={{ display:'flex', justifyContent:'center', alignItems: 'center', minHeight: '100vh'}}>
-      <Card sx={{ maxWidth: 1000, bgcolor: 'primary.main', color: 'primary.contrastText' }}>
+    <Box
+      component="section"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Card
+        sx={{
+          maxWidth: 1000,
+          bgcolor: "primary.main",
+          color: "primary.contrastText",
+          mt: 4,
+          mb: 4,
+        }}
+      >
         <CardActionArea>
           <CardMedia
             component="img"
@@ -22,9 +37,15 @@ export default function Home() {
             <Typography gutterBottom variant="h5" component="div">
               FRONTEND DEVELOPER
             </Typography>
-            <Typography variant="body2" sx={{ color: 'text.contrastText', textAlign:'justify'}}>
-              Ingeniero en Sistemas con 4 años de experiencia desarrollando soluciones web robustas con Laravel, React y Lit Element. Experto en bases de datos SQL, 
-              despliegue tanto en Digital Ocean como en servidores Dabian y soporte técnico integral. Certificado por Microsoft en CRM Dynamics 365. Alta capacidad de adaptación, 
+            <Typography
+              variant="body2"
+              sx={{ color: "text.contrastText", textAlign: "justify" }}
+            >
+              Ingeniero en Sistemas con 4 años de experiencia desarrollando
+              soluciones web robustas con Laravel, React y Lit Element. Experto
+              en bases de datos SQL, despliegue tanto en Digital Ocean como en
+              servidores Dabian y soporte técnico integral. Certificado por
+              Microsoft en CRM Dynamics 365. Alta capacidad de adaptación,
               trabajo en equipo y resolución de problemas.
             </Typography>
           </CardContent>
