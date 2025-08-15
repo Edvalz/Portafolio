63f4b2108679ba089923ca19cc5adc3800c2f45b
Edvalz <eduardo_valllejo@outlook.es>
2025-08-15 16:41:03 -0600

    refactor: se rediseña la pagina principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import CardActionArea from "@mui/material/CardActionArea";
-import Box from "@mui/material/Box";
-import Link from "next/link";
 import { useState, useEffect } from "react";
-import { Slide } from "@mui/material";
+import SoftItem from "./cv/SoftItem";
+import {
+  Box,
+  Slide,
+  Typography,
+  Grid,
+  Paper,
+  Avatar,
+  Stack,
+} from "@mui/material";
 
 export default function Home() {
   const [show, setShow] = useState<boolean>(false);
@@ -22,46 +24,99 @@ export default function Home() {
       component="section"
       sx={{
         display: "flex",
-        justifyContent: "center",
+        minHeight: "90vh",
         alignItems: "center",
-        minHeight: "100vh",
-        mt: 10,
+        justifyContent: "center",
+        justifyItems: "center",
+        p: 2,
+        mt: 7,
       }}
     >
       <Slide in={show} direction="down" timeout={1500}>
-        <Card
+        <Paper
+          elevation={3}
           sx={{
-            maxWidth: 1000,
-            bgcolor: "primary.main",
-            color: "primary.contrastText",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "75%",
+            bgcolor: "#171717",
+            p: 4,
+            mt: 2,
             mb: 4,
           }}
         >
-          <CardActionArea component={Link} href="/cv">
-            <CardMedia
-              component="img"
-              height="500"
-              image="/images/portada.jpg"
-              alt="Portada"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                FRONTEND DEVELOPER
-              </Typography>
-              <Typography
-                variant="body2"
-                sx={{ color: "text.contrastText", textAlign: "justify" }}
+          <Grid container spacing={2} sx={{ display: "flex", width: "100%" }}>
+            <Grid
+              size={{ xs: 12, sm: 4 }}
+              sx={{ display: "flex", alignItems: "center" }}
+            >
+              <Avatar
+                alt="Remy Sharp"
+                src="/images/foto.webp"
+                sx={{
+                  width: { xs: 150, sm: 200, md: 250 },
+                  height: { xs: 150, sm: 200, md: 250 },
+                  border: "4px solid",
+                  borderColor: "primary.main",
+                  boxShadow: 4,
+                  margin: "0 auto",
+                }}
+              />
+            </Grid>
+            <Grid
+              size={{ xs: 12, sm: 8 }}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <Grid
+                container
+                spacing={4}
+                sx={{ width: "100%", color: "primary.contrastText" }}
               >
-                Ingeniero en Sistemas con 4 años de experiencia desarrollando
-                soluciones web robustas con Laravel, React y Lit Element.
-                Experto en bases de datos SQL, despliegue tanto en Digital Ocean
-                como en servidores Dabian y soporte técnico integral.
-                Certificado por Microsoft en CRM Dynamics 365. Alta capacidad de
-                adaptación, trabajo en equipo y resolución de problemas.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+                <Grid size={12}>
+                  <Stack>
+                    <Typography variant="h5" align="center">
+                      ¿Quieres conocerme?
+                    </Typography>
+                    <Typography variant="body2" align="justify" sx={{ mt: 2 }}>
+                      Mi nombre es Eduardo y tengo el título Ingeniero en
+                      Sistemas especializado en desarrollo web con experiencia
+                      en Laravel, React y arquitectura de software. Apasionado
+                      por crear soluciones eficientes, escalables y fáciles de
+                      usar.
+                    </Typography>
+                  </Stack>
+                </Grid>
+                <Grid size={12}>
+                  <Typography variant="h5" align="center" sx={{ mb: 2 }}>
+                    Teconologías con las que he trabajado
+                  </Typography>
+                  <SoftItem
+                    skills={[
+                      "Html",
+                      "CSS",
+                      "Javascript",
+                      "React",
+                      "Vue",
+                      "PHP",
+                      "Blade",
+                      "Laravel",
+                      "Git",
+                      "Github",
+                      "Dynamics 365",
+                      "Wordpress",
+                      "C#",
+                      "MySQL",
+                      "PostgresSQL",
+                      "MariaDB",
+                      "Linux",
+                    ]}
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
       </Slide>
     </Box>
   );
